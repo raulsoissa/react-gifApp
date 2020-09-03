b35b023360181b6cafb012f2a62a4c93f2b991e6
raulsoissa <raulsoissa@gmail.com>
2020-09-03 13:22:46 -0400

    docs add

---
+++ docs/precache-manifest.c0d266412dff553a9af73bbdf3447b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5ff4149b87e8354d3e24871ab606d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc48a32b0c2853462a3b",
+    "url": "/static/css/main.16a40ab8.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc48a32b0c2853462a3b",
+    "url": "/static/js/main.c9c14913.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
